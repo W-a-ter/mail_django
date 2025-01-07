2d48f1fc18c3515704785548a9cf73d23ab8e2ac
verba4182 <verba4182@mail.ru>
2025-01-07 17:05:04 +0300

    made some html homepage heed to be chandged

--- spam_mailing/views.py
+++ spam_mailing/views.py
@@ -6,6 +6,24 @@ from spam_mailing.models import Receiver, Mailing
 class HomeView(ListView):
     model = Mailing
     template_name = 'spam_mailing/home.html'
+    context_object_name = 'mailing'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["user_mail"] = UserMail.objects.all()
+        context["mailing_all_started"] = Mailing.objects.filter(status="Запущена")
+
+        if self.request.user.is_authenticated:
+            context['user_usermail'] = UserMail.objects.filter(owner=self.request.user)
+            context['user_mailing_started'] = Mailing.objects.filter(owner=self.request.user, status='Запущена')
+            context['user_mailing'] = Mailing.objects.filter(owner=self.request.user)
+
+        return context
+
+    def get_queryset(self):
+        """Настройка серверного кэширования главной страницы"""
+        return GetListMailing.get_list_mailing_from_cache()
 
 
 class ReceiverCreateView(CreateView):
